cc9e5c6401a0cec90a12d3a5f5e4e5d4bed5104b
ABIOLA <abiolarashidi@gmail.com>
2021-05-02 18:05:21 +0300

    Add project files.

---
+++ ATM Application.cpp
@@ -0,0 +1,63 @@
+// ATM Application.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+using std::cout;
+using std::cin;
+
+//LOGIN AND MENU
+void login() {
+
+    int userPin = 1234, Pin, errCounter = 0, attempt;
+    cout << "You have 3 Login Attempts" << endl;
+    do {
+        cout << "Enter PIN :";
+        cin >> Pin;
+        system("cls");
+
+        if (Pin != userPin)
+            errCounter++;
+        attempt = errCounter;
+        cout << "attempt : " << attempt << endl;
+
+
+    } while (errCounter < 3 && Pin != userPin);
+    if (errCounter < 3){
+        cout << "              Welcome BBT Bank" << endl;
+
+                cout << "           BBT-Bank" << endl;
+
+            cout << "********************MENU*****************" << endl;
+            cout << "*>> 1. Check Balance                      *" << endl;
+            cout << "*                                         *" << endl;
+            cout << "*>> 2. Withdraw Amount                    *" << endl;
+            cout << "*                                         *" << endl;
+            cout << "*>> 3. Deposite Amount                    *" << endl;
+            cout << "*                                         *" << endl;
+            cout << "*>> 4. Transfer Amount                    *" << endl;
+            cout << "*                                         *" << endl;
+            cout << "*>> 5. Exit                               *" << endl;
+            cout << "******************************************" << endl;
+    }else {
+        cout << "Blocked" << endl;
+    }
+        
+
+
+
+}
+
+
+
+int main() {
+
+
+    login();
+    
+
+    system("pause");
+
+}
